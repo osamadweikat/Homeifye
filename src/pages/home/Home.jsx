7b0d13c73c663b443047f3bbec913504e0de69c5
osamadweikat <osamadweikat83@gmail.com>
2025-10-20 21:54:17 +0300

    add search property to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,50 @@
 import "./home.css";
 import LogoImage from "../../assets/images/logo.svg";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import SearchIcon from "@mui/icons-material/Search";
 import Button from "@mui/material/Button";
 import HomeHeroImage from "../../assets/images/home-hero.avif";
 import RightArrow from "../../assets/images/right arrow.svg";
-import { useState } from "react";
+import HomeIcon from "..//../assets/images/home-icon.svg";
+import LocationIcon from "..//../assets/images/location-icon.svg";
+import TypesIcon from "..//../assets/images/types-icon.svg";
+import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
   const [openDropdown, setOpenDropdown] = useState(false);
+  const searchRef = useRef(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const currentRef = searchRef.current;
+    let userHasScrolled = false;
+
+    const handleScroll = () => {
+      userHasScrolled = true;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && userHasScrolled) {
+            setVisible(true);
+            observer.disconnect();
+            window.removeEventListener("scroll", handleScroll);
+          }
+        });
+      },
+      { threshold: 0.15 }
+    );
+
+    if (currentRef) observer.observe(currentRef);
+
+    return () => {
+      if (currentRef) observer.unobserve(currentRef);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className="home">
@@ -126,6 +163,50 @@ export default function Home() {
               src={HomeHeroImage}
               alt="home-hero-image"
             />
+          </div>
+        </div>
+      </div>
+      <div className="search-property">
+        <div className="search-property-padding">
+          <div className="search-property-container">
+            <div className="search-property-inner" ref={searchRef}>
+              <div className="property-search-block">
+                <div
+                  className={`property-search-bar-wrapper ${
+                    visible ? "visible" : ""
+                  }`}
+                >
+                  <SearchIcon className="search-icon" />
+                  <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search for properties"
+                  />
+                  <button>Search</button>
+                </div>
+                <div
+                  className={`property-search-drop-downs ${
+                    visible ? "visible" : ""
+                  }`}
+                >
+                  <div className="property-search-drop-down-item">
+                    <img src={HomeIcon} alt="home-icone" />
+                    <span>Properties</span>
+                    <ExpandMoreIcon sx={{ marginLeft: "15px" }} />
+                  </div>
+                  <div className="property-search-drop-down-item">
+                    <img src={LocationIcon} alt="location-icone" />
+                    <span>Location</span>
+                    <ExpandMoreIcon sx={{ marginLeft: "15px" }} />
+                  </div>
+                  <div className="property-search-drop-down-item">
+                    <img src={TypesIcon} alt="type-icone" />
+                    <span>Types</span>
+                    <ExpandMoreIcon sx={{ marginLeft: "15px" }} />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
