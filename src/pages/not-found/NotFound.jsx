e8dcaeab6194c28e7cbda3d341b6652648cfb0a8
osamadweikat <osamadweikat83@gmail.com>
2025-10-26 12:53:33 +0200

    create not found page

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,7 +1,14 @@
 import "./not-found.css"
+import RightArrow from "../../assets/images/right arrow.svg";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="not-found-section">
+        <div className="not-found-container">
+            <h1>404</h1>
+            <p>Hmmmm... I couldn't find that page.</p>
+            <button>Go To Home Page <img src={RightArrow} alt="arrow" /></button>
+        </div>
+    </div>
   )
 }
