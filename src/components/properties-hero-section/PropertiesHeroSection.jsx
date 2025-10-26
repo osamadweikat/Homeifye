443fadade7b7e4d8898677929b5a3c3d31820ee3
osamadweikat <osamadweikat83@gmail.com>
2025-10-26 21:19:12 +0200

    add properties hero section styles

--- src/components/properties-hero-section/PropertiesHeroSection.jsx
+++ src/components/properties-hero-section/PropertiesHeroSection.jsx
@@ -1,19 +1,31 @@
 import "./properties-hero-section.css";
+import SearchProperty from "../search-property/SearchProperty";
+import useReloadOnSameRoute from "../../hooks/useReloadOnSameRoute";
+import { Link } from "react-router-dom";
+import useInViewObserver from "../../hooks/useInViewObserver";
 
 export default function PropertiesHeroSection() {
+  const reloadIfSame = useReloadOnSameRoute();
+
+  useInViewObserver(".fade-section, .fade-title", { threshold: 0.2 }, true);
+
   return (
     <div className="properties-hero-section">
-      <div className="properties-padding-section">
-        <div className="properties-padding">
-          <div className="properties-container">
-            <div className="properties-breadcrumbs">
-              <span>Home / </span>
+      <div className="properties-padding">
+        <div className="properties-container">
+          <div className="properties-breadcrumbs fade-section">
+            <Link to="/">
+              <span>Home</span>
+            </Link>
+            /
+            <Link to="/properties" onClick={() => reloadIfSame("/properties")}>
               <span>Properties</span>
-            </div>
-            <h1>Properties</h1>
+            </Link>
           </div>
+          <h1 className="fade-title">Properties</h1>
         </div>
       </div>
+      <SearchProperty />
     </div>
   );
 }
