8b0840a9ed0458dfce3b7c010ac5278d108990e1
osamadweikat <osamadweikat83@gmail.com>
2025-10-30 21:36:33 +0200

    make property plan section full responsive

--- src/components/property-plan-section/PropertyPlanSection.jsx
+++ src/components/property-plan-section/PropertyPlanSection.jsx
@@ -1,8 +1,15 @@
+import useInViewObserver from "../../hooks/useInViewObserver";
 import "./property-plan-section.css";
 import RightArrow from "../../assets/images/right arrow.svg";
 import MapImage from "../../assets/images/maps.avif";
 
 export default function PropertyPlanSection() {
+  useInViewObserver(
+    ".property-plan-heading h2, .property-plan-header p, .property-plan-header button, .property-plan-image-wrapper",
+    { threshold: 0.3 },
+    true
+  );
+
   return (
     <div className="property-plan-section-padding">
       <div className="property-plan-section-container">
