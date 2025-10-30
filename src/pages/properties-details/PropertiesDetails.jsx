0d0669825c39716d1617e250525817eb6d6ec7b2
osamadweikat <osamadweikat83@gmail.com>
2025-10-30 22:46:56 +0200

    add similar listings and global cta sections to properties details page

--- src/pages/properties-details/PropertiesDetails.jsx
+++ src/pages/properties-details/PropertiesDetails.jsx
@@ -3,14 +3,31 @@ import PropertyFeatureSection from "../../components/property-feature-section/Pr
 import PropertyInfoSection from "../../components/property-info-section/PropertyInfoSection";
 import PropertyPlanSection from "../../components/property-plan-section/PropertyPlanSection";
 import "./properties-details.css";
+import { propertiesData } from "../../data/propertiesData";
+import NewListingInner from "../../components/new-listing-inner/NewListingInner";
+import useInViewObserver from "../../hooks/useInViewObserver";
+import GlobalCtaSection from "../../components/global-cta-section/GlobalCtaSection";
 
 export default function PropertiesDetails() {
+  const similarListings = propertiesData.slice(-3)
+
+  useInViewObserver(
+      ".similar-listings-header h2",
+      { threshold: 0.3 },
+      true
+    );
+
   return (
     <div className="properties-details">
       <PropertiesDetailsHeroSection />
       <PropertyFeatureSection />
       <PropertyInfoSection />
       <PropertyPlanSection />
+      <div className="similar-listings-header">
+        <h2>Similar Listings</h2>
+      </div>
+      <NewListingInner variant="properties" data={similarListings} />
+      <GlobalCtaSection />
     </div>
   );
 }
